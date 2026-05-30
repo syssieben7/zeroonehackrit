085c4b1403b3fccdbdb5abbf48658e28950b494c
Adrian Top <65786215+konkentrate@users.noreply.github.com>
2026-05-30 05:48:38 +0200

    Update slurm.sh

--- transformer/slurm.sh
+++ transformer/slurm.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=boost_usr_prod
-#SBATCH --reservation=s_tra_ncc
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-task=1
-#SBATCH --mem=32GB
-#SBATCH --cpus-per-task=8
-#SBATCH --time=01:00:00
-#SBATCH --job-name=zeronehack-train
-#SBATCH --output=logs/%j.out
-#SBATCH --error=logs/%j.err
-
-mkdir -p logs
-
-cd $SLURM_SUBMIT_DIR
+#SBATCH --reservation=s_tra_ncc # Reservation for the hackathon
+#SBATCH --nodes=1 # Number of nodes
+#SBATCH --ntasks-per-node=1 # Number of `srun` tasks executed per node
+#SBATCH --gpus-per-task=1 # Number of GPUs (up to 4 on Leonardo)
+#SBATCH --mem=10GB # Fair share on Leonardo: 120GB * gpus-per-task
+#SBATCH --cpus-per-task=2 # Fair share on Leonardo: 8 * gpus-per-task
+#SBATCH --time=00:00:10 # Time limit in HH:MM:SS, up to 24:00:00
 
 pixi run -m transformer python transformer/train.py \
     --model t5-small \
